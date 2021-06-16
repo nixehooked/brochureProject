e92f4a854938cdfc2c75b530e2842ba93ade7b49
nixehooked <nixedu06@gmail.com>
2021-06-16 13:52:13 +0200

    Ajout d'un formulaire test de connexion d'utilisateur avec la redirection après connexion et la redirection après la deconnexion

--- brochure/brochure/urls.py
+++ brochure/brochure/urls.py
@@ -14,14 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.blog_view, name='blog'),
+    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
     path('<int:id>/', views.detail_view, name='detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
